b065dc4f9e7f2864ff536d30404f176570734559
Lucas <lucas.rouillermonay@edueptm.ch>
2024-03-10 19:35:42 +0100

    Update card design layout

--- components/Card.vue
+++ components/Card.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+import {
+  LinkIcon,
+  ArrowTopRightOnSquareIcon,
+  StarIcon,
+} from "@heroicons/vue/20/solid";
+
 const props = defineProps<{
   user?: ProfileData;
   success?: boolean;
@@ -10,7 +16,7 @@ const { user, success } = props;
 <template>
   <div
     v-if="success"
-    class="flex flex-col md:flex-row rounded-3xl bg-white cursor-pointer overflow-hidden [transition:all_0.3s] max-w-full min-w-[20rem] min-h-[17.5rem] shadow-indigo-500 shadow-md md:hover:shadow-lg md:hover:shadow-indigo-300 md:hover:scale-105 md:hover:rotate-2 ease-out"
+    class="flex flex-col md:flex-row rounded-3xl bg-white cursor-pointer overflow-hidden [transition:all_0.3s] max-w-full min-w-[20rem] min-h-[17.5rem] shadow-neutral-500 shadow-md md:hover:shadow-lg md:hover:shadow-neutral-300 md:hover:scale-105 md:hover:rotate-2 ease-out"
   >
     <div class="flex">
       <NuxtImg
@@ -29,11 +35,18 @@ const { user, success } = props;
       />
     </div>
     <div class="flex flex-col justify-start p-4">
-      <h2 class="text-2xl font-bold">
+      <h2
+        class="flex items-center gap-x-1 text-lg md:text-3xl font-bold mt-auto"
+      >
         {{ user?.displayName || user?.username }}
-        <span v-if="user && user.accountAge > '1'"
+        <span class="text-xs md:text-lg" v-if="user && user.accountAge > '1'"
           >({{ user?.accountAge }} years old)</span
         >
+        <StarIcon
+          v-if="user?.id === '265896171384340480'"
+          class="text-amber-400 -ml-0.5 h-5 w-5"
+          aria-hidden="true"
+        />
       </h2>
       <div class="flex gap-x-4 my-4">
         <NuxtImg
@@ -45,24 +58,26 @@ const { user, success } = props;
         />
       </div>
       <div class="mt-auto">
-        <p class="font-semibold text-sm">
-          {{ user?.createdAt }}
-        </p>
-        <div class="border-t-2 pt-4 gap-3 flex flex-wrap">
+        <div class="flex flex-col md:flex-row border-t-2 pt-4 gap-3">
           <NuxtLink
             :href="`discord://-/users/${user?.id}`"
             rel="noopener noreferrer"
-            class="flex-[1_0_15rem] flex justify-center items-center whitespace-nowrap max-w-full shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+            class="flex gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
+            <ArrowTopRightOnSquareIcon
+              class="-ml-0.5 h-5 w-5"
+              aria-hidden="true"
+            />
             View on Discord
           </NuxtLink>
           <NuxtLink
             :href="`https://discord.name/${user?.id}`"
             target="_blank"
             rel="noopener noreferrer"
-            class="flex-[1_0_05rem] flex justify-center items-center whitespace-nowrap max-w-full shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+            class="flex gap-x-1.5 rounded-md bg-neutral-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-neutral-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-600"
           >
-            More info
+            <LinkIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+            View on Discord.Name
           </NuxtLink>
         </div>
       </div>
